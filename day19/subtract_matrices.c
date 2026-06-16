6cdb271c59bc3ee4ba71dde3ffc9884d440ec3a9
Adarsh Kumar <adarshk050308@gmail.com>
2026-06-16 16:56:26 +0530

    Add matrix subtraction program in C

---
+++ day19/subtract_matrices.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main() {
+    int rows, cols;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter the number of columns: ");
+    scanf("%d", &cols);
+    
+    int matrix1[rows][cols], matrix2[rows][cols], result[rows][cols];
+    
+    printf("Enter the elements of the first matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &matrix1[i][j]);
+        }
+    }
+    
+    printf("Enter the elements of the second matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &matrix2[i][j]);
+        }
+    }
+    
+    // subtracting the two matrices
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            result[i][j] = matrix1[i][j] - matrix2[i][j];
+        }
+    }
+    
+    printf("Result of subtraction:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d  ", result[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
